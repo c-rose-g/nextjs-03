f1ceb037637d3b2e5edb17bd5044dd318f96deb6
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-03-26 19:17:15 -0700

    Import getFeaturedEvents from dummy data into pages index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-const HomePage = () =>{
-  return(
-    <div>
-      <h1>
-        The Home Page.
-      </h1>
-    </div>
-  )
-}
+import { getFeaturedEvents } from '../dummy-data';
+const HomePage = () => {
+  // access function inside dummy data
+	const featuredEvents = getFeaturedEvents();
+	return (
+		<div>
+    <ul></ul>
+		</div>
+	);
+};
 export default HomePage;
