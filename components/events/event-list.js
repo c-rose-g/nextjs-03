fe76e7c3e205f092b5013647b0d2799d17fe2b13
Cindy R Guzman <97000116+c-rose-g@users.noreply.github.com>
2023-03-27 09:57:01 -0700

    add props to items map in event list component

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,12 +1,20 @@
 // file name can be either in PascalCase or kabob-case
 // doesn't care where the events are coming from, only outputting the events
 // recieves props just to display/render from where the event data is fetched
-import { EventItem } from './event-item'
+import { EventItem } from './event-item';
 const EventList = (props) => {
 	const { items } = props;
 	return (
 		<ul>
-			{items.map(event => <EventItem/>)}
+			{items.map((event) => (
+				<EventItem
+					key={event.id}
+					id={event.id}
+					title={event.title}
+					date={event.date}
+					image={event.image}
+				/>
+			))}
 		</ul>
 	);
 };
